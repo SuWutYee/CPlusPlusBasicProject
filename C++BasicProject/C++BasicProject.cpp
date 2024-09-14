096b094221f8f3c1132fc73e0270ffbf9515ed38
Su Wut Yee <suwutyeenaing@gmail.com>
2024-09-14 16:34:43 +0630

    If..else switch

--- C++BasicProject/C++BasicProject.cpp
+++ C++BasicProject/C++BasicProject.cpp
@@ -2,10 +2,85 @@
 //
 
 #include <iostream>
+#include <string>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	// double value testing
+
+	/*double d1 = 12E6;
+	std::cout << "Hello World!\n";
+	std::cout << "value is \t" << (uint64_t)d1;*/
+
+	// char value testing
+
+	/*char a = 97, b = 66, c = 67;
+	std::cout << a;
+	std::cout << b;
+	std::cout << c;*/
+
+	// '/' , '%' value testing
+
+	/*int x = 5;
+	int a = 3;
+	std::cout << x / a;*/
+	//std::cout << x % a;
+
+	/*int x = 5;
+	x &= 3;
+	std::cout << x;*/
+
+	/*int x, y, z;
+	x = 5, y = 6, z = 50;
+	std::cout << x + y + z;*/
+
+	/*int a = 2;
+	int b = 5;
+	string result;
+	if (a > b)
+	{
+		std::cout << "True \n";
+	}
+	else
+	{
+		std::cout << "False \n";
+	}
+
+	result = (a > b) ? "True" : "False";
+	cout << result;*/
+
+	string result = "";
+	int day = 5;
+	/*if (day == 1)
+	{
+		result = "Monday";
+	}
+	else if (day == 2)
+	{
+		result = "Tuesday";
+	}
+	else if (day == 3)
+	{
+		result = "Wednesday";
+	}*/
+
+	switch (day)
+	{
+	case 1:
+		result = "Monday";
+		break;
+	case 2:
+		result = "Tuesday";
+		break;
+	default:
+		result = "Other";
+		break;
+	}
+
+	cout << result;
+
+	return 0;
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
