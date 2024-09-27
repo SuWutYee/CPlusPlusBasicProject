946871976a41649b769cd25de5ad3601e9311ea9
Su Wut Yee <suwutyeenaing@gmail.com>
2024-09-27 14:35:57 +0630

    feat: testing homework loop

--- C++BasicProject/C++BasicProject.cpp
+++ C++BasicProject/C++BasicProject.cpp
@@ -1,12 +1,31 @@
-#include "MainHeader.h"
+﻿#include "MainHeader.h"
 #include <string>
 #include <iostream>
 using namespace std;
 
 int main()
 {
-	int x;
+	/*int x;
 	Homework_Condition();
 	cin >> x;
-	return 0;
+	return 0;*/
+
+    char one[] = "one";
+    char two[] = "two";
+
+    cout << strcmp(one, two) << "\n";
+
+    if (one == two) {
+        cout << "Equal";
+    }
+
+    if (strcmp(one, two) == 0) {
+        cout << "Equal";
+    }
+    else
+    {
+        cout << "NotEqual";
+    }
+
+    return 0;
 }
